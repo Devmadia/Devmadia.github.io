56bc0c15c4bf4c2377d0094ac92dd727680f7265
Devmadia <agathaphan@icloud.com>
2020-11-03 21:37:47 -0600

    updated header, created footer, styled

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            
+            <footer>
+                <div className="row">
+                <div className="three columns">
+                    <ul className="socstyle">
+                    <li><a href="#"><i className="fa fa-linkedin" /></a></li>
+                    <li><a href="#"><i class="fab fa-github"></i></a></li> DEFAULT FOR ICONS
+                    </ul>
+                    <ul className="copyright">
+                    <li><i class="far fa-copyright"></i> 2020 Devmadia </li>
+                     
+                    </ul>
+                </div>
+                <div id="go-top"><a className="smoothscroll" title="Take Me Home" href="#home"><i class="fas fa-broom"></i></a></div>
+                </div>
+            </footer>
         );
     }
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            
+            <header id="home">
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                    <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                    <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Work</a></li>
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                    </ul> {/* end #nav */}
+                </nav> {/* end #nav-wrap */}
+                <div className="row banner">
+                    <div className="banner-text">
+                    <h1 className="responsive-headline">I'm Devmadia.</h1>
+                    <hr />
+                    <h3>A full stack <span>web developer</span>, <span>writer</span> and <span>gamer</span> with a love of cultivating carnivorous plants. Feel free to <a className="smoothscroll" href="#about">explore </a>
+                        a bit and learn <a className="smoothscroll" href="#about">about me</a> as you go!</h3>
+                    <hr />
+                    </div>
+                </div>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i class="fas fa-chevron-circle-down"></i></a>
+                </p>
+            </header>
         );
     }
 }
