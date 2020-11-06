b489164fdeafae20247350207d6595a31b025e0f
Devmadia <agathaphan@icloud.com>
2020-11-06 17:28:36 -0600

    Updates

---
+++ precache-manifest.8c9f27abdbfbd03f6f86e3fdc05b44ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577b56330e270813d29146a4f05de640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f11971710e6cf0fc1ca",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dde43db8fd4dc1938f83",
+    "url": "/static/js/2.50ae8ce6.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.50ae8ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f11971710e6cf0fc1ca",
+    "url": "/static/js/main.d1238b81.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
